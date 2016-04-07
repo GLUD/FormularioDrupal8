8c0ee9359c698994978ce63ea571626af62d5b62
Jorge Useche <naturalmentejorge@gmail.com>
2016-04-07 14:39:28 -0500

    Se agrega el código funcional del módulo

---
+++ Modtest/src/Form/ContributeForm.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\amazing_forms\Form\ContributeForm.
+ */
+
+namespace Drupal\amazing_forms\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+
+/**
+ * Contribute form.
+ */
+class ContributeForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+}
+?>
